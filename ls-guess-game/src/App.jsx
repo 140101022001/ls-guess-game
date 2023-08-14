73a30cf6eca903b8f4290ed106b5de8a22b1f379
Nguyen Van Hung <nv-hung@linkstaff.co.jp>
2023-08-14 12:51:46 +0900

    08/14

--- ls-guess-game/src/App.jsx
+++ ls-guess-game/src/App.jsx
@@ -1,34 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useRef, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [arrayGuessNumber, setArrayGuessNumber] = useState([]);
+  const [result, setResult] = useState(false);
+  const [message,setMessage] = useState("");
+  const [disable, setDisable] = useState(false);
+  const [newGame, setNewGame] = useState(false);
+  const guessNumber = useRef();
+  
+  const [randomNumber, setRandomNumber] = useState(0);
 
+  useEffect(() => {
+    setRandomNumber(Math.floor(Math.random() * 100) + 1);
+  }, [newGame]);
+  const playGame = () => {
+    if (guessNumber.current.value < 1 || guessNumber.current.value> 100) {
+      setMessage('Please enter a number from 1 to 100.')
+    } else {
+      if (arrayGuessNumber.length < 4) {
+        const newArray = [...arrayGuessNumber, guessNumber.current.value];
+        setArrayGuessNumber(newArray);
+        if (guessNumber.current.value == randomNumber) {
+          setResult(true);
+          setDisable(true);
+        } else {
+          setResult(false);
+          if (guessNumber.current.value > randomNumber) {
+            setMessage('大きいすぎです。')
+          } else if (guessNumber.current.value < randomNumber) {
+            setMessage('小さいすぎです。')
+          }
+        }
+      }
+    }
+    guessNumber.current.value = "";
+  }
+  const startNewGame = () => {
+    setNewGame(true);
+    setArrayGuessNumber([]);
+    setResult(false);
+    setMessage("");
+    setDisable(false);
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='container'>
+      <div className='box'>
+        <div className='header'>
+          <p>{randomNumber}</p>
+          <h1>Number Guessing Game</h1>
+        </div>
+        <div className='body'>
+          <p>We have selected a random number between 1 and 100. See if you can guess it in 4
+          turns or fewer. We&apos;ll tell you if your guess was too high or too low.</p>
+          <div className='input-field'>
+            <p>Enter a guess:</p>
+            <input type="number" name='guess-number' className='guess-number' ref={guessNumber} disabled={disable||arrayGuessNumber.length == 4}/>
+            <button type='button' onClick={() => playGame()}>Submit guess</button>
+          </div>
+        </div>
+        <div className='footer'>
+          <p>Previous guesses:{arrayGuessNumber.join(',')}</p>
+          {!result && message && arrayGuessNumber.length < 4 &&<p className='message red'>WRONG!</p>}
+          {!result && arrayGuessNumber.length >= 4 && <p className='message black'>Game Over!</p>}
+          {result && <p className='message green'>Congratulations!You got it right!</p>}
+          {message && <p>{message}</p>}
+          {!result && arrayGuessNumber.length >= 4 &&
+          <button type='button' onClick={startNewGame}>New game</button>}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
